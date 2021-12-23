2cbfade6af602116421e323f5e6d1e623ee73b6a
sshar1 <96096653+sshar1@users.noreply.github.com>
2021-12-23 18:30:17 -0500

    Update Powercell_Photo_Detector.py

--- Powercell_Photo_Detector.py
+++ Powercell_Photo_Detector.py
@@ -68,7 +68,7 @@ def drawRectangle(contours, image):
 
 
 #Draw the rectangles
-editImage("powercelltest.jpg", [22, 93, 0], [45, 255, 255])
+editImage("/Images/frcpowercells.jpg", [22, 93, 0], [45, 255, 255])
 drawRectangle(mask_cnts, original_resize)
             
 cv2.imshow("masked", mask)
